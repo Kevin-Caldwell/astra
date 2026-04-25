21542cf0069cd59577b2aaeea688b96547a0e0cb
Kevin Caldwell <kevinc2004@gmail.com>
2026-04-24 21:04:37 -0400

    Add integer extend modules

---
+++ utils/signed_extend.sv
@@ -0,0 +1,23 @@
+
+module Sext #(
+  parameter inLen = 8, 
+  parameter outLen = 32
+) (
+  input logic[inLen-1:0] in, 
+  output logic[outLen-1:0] out
+);
+  parameter extLen = outLen - inLen;
+  logic highest = in[inLen-1];
+  assign out <= {extLen{highest}, in};
+endmodule
+
+module Zext #(
+  parameter inLen = 8, 
+  parameter outLen = 32
+) (
+  input logic[inLen-1:0] in, 
+  output logic[outLen-1:0] out
+);
+  parameter extLen = outLen - inLen;
+  assign out <= {extLen{1'b0}, in};
+endmodule
