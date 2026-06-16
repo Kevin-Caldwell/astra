970fe117252a1c0657b2ef810012203b6a5148a9
Kevin Caldwell <kevinc2004@gmail.com>
2026-06-15 22:46:42 -0400

    Add pipeline signals

--- hw/core/pipeline_stages/pipeline_signals.sv
+++ hw/core/pipeline_stages/pipeline_signals.sv
@@ -1,28 +1,67 @@
 `include "defs.sv"
 
 typedef struct packed {
-  
-} CommonStageSignals_s;
-
-typedef struct packed {
-  reg_addr_t write_address, 
-  reg_addr_t read_address_a, 
+  // Register
+  reg_addr_t write_address,
+  reg_addr_t read_address_a,
   reg_addr_t read_address_b,
-  bus_t write_value, 
-  logic use_immediate,
+  reg_addr_t write_value,
+  logic use_immedate,
+
+  // ALU
   logic negate,
-} RegisterFileStageSignals_s;
+  reg_addr_t alu_input_a,
+  reg_addr_t alu_input_b,
+  reg_addr_t alu_output,
+
+  // Update Program Counter
+  logic ctrl_instruction,
+  logic branch_success,
+  bus_t old_pc,
+  bus_t offset,
+  bus_t new_pc
+
+} PipelineSignals;
+
 
 typedef struct packed {
-  bus_t a, 
-  bus_t b, 
-  logic[2:0] op
-} ALUStageSignals_s;
+  instr_t instruction,
+  bus_t pc,
+} fetchPipeline_s;
 
 typedef struct packed {
-  
-} MemoryStageSignals_s;
+  instr_t instruction,
+  bus_t pc,
+  logic instruction_type, // EDIT
+} decodePipeline_s;
 
 typedef struct packed {
-  
-} PCUpdateStage;
+  instr_t instruction,
+  bus_t pc,
+  bus_t rf_a,
+  bus_t rf_b,
+} rfPipeline_s;
+
+typedef struct packed {
+  instr_t instruction,
+  bus_t pc,
+  bus_t alu_out,
+  ALUFlags alu_flags,
+} aluPipeline_s;
+
+typedef struct packed {
+
+} memoryPipeline_s;
+
+typedef struct packed {
+  instr_t instruction,
+  bus_t pc,
+} pcUpdatePipeline_s;
+
+typedef struct packed {
+  instr_t instruction,
+  bus_t pc,
+  bus_t new_pc,
+  bus_t rf_w,
+  bus_t rf_wb,
+} writeBackPipeline_s;
